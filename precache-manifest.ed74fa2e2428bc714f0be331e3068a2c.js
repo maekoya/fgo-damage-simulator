3f186ef8ff278351ef8215577c97d6642fc488b3
h-maekoya <h-maekoya@colsis.jp>
2018-09-21 16:10:59 +0900

    Updates

---
+++ precache-manifest.ed74fa2e2428bc714f0be331e3068a2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62705dd4bd040231f44d",
+    "url": "/js/chunk-vendors.a5715fb5.js"
+  },
+  {
+    "revision": "851ae1476b6e1959576f",
+    "url": "/js/app.ce39891f.js"
+  },
+  {
+    "revision": "2a554b879fcf442f616ddcb92e6977da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62705dd4bd040231f44d",
+    "url": "/css/chunk-vendors.d5961043.css"
+  },
+  {
+    "revision": "851ae1476b6e1959576f",
+    "url": "/css/app.165f5364.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,21 @@
+importScripts("/precache-manifest.ed74fa2e2428bc714f0be331e3068a2c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ */
+
+workbox.core.setCacheNameDetails({prefix: "FGO-simulator"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
