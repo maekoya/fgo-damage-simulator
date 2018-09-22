627b124c2e16ca4e9ace71c32ddffbb5e914f567
h-maekoya <h-maekoya@colsis.jp>
2018-09-22 18:09:45 +0900

    Updates

---
+++ precache-manifest.62ecfb05b52a0e78442fb2e13ef88679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62705dd4bd040231f44d",
+    "url": "js/chunk-vendors.a5715fb5.js"
+  },
+  {
+    "revision": "49c3f38428c31c29fa7f",
+    "url": "js/app.485096d6.js"
+  },
+  {
+    "revision": "fe320e27d3856736bc6e247420e7b9a7",
+    "url": "index.html"
+  },
+  {
+    "revision": "62705dd4bd040231f44d",
+    "url": "css/chunk-vendors.d5961043.css"
+  },
+  {
+    "revision": "49c3f38428c31c29fa7f",
+    "url": "css/app.165f5364.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.66f2fa030728f0d063fc874642dd2f48.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+importScripts("precache-manifest.62ecfb05b52a0e78442fb2e13ef88679.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
 
 /**
  * Welcome to your Workbox-powered service worker!
